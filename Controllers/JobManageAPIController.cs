15a0156db38c05e6077afee26b03bb1e3719a540
Santosh Singh <santosh3033@gmail.com>
2023-01-12 11:49:00 +0530

    Added JobManageAPIController

---
+++ Controllers/JobManageAPIController.cs
@@ -0,0 +1,38 @@
+﻿using JobManagement.Models.Dto;
+using JobManagement.Repository.AccountServices;
+using JobManagement.Repository.IAccountServices;
+using JobManagement.Repository.IJobManageServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobManageAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IJobManageRepository _jobManageRepository;
+        public JobManageAPIController(ResponseDto response, IJobManageRepository jobManageRepository)
+        {
+            _response = response;
+            _jobManageRepository = jobManageRepository;
+        }
+        [HttpPost]
+        [Route("createjob")]
+        public async Task<object> Post([FromBody] JobDetailDto jobDetailDto)
+        {
+            try
+            {
+                string result = await _jobManageRepository.CreateJob(jobDetailDto);
+                _response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message.ToString() };
+            }
+            return _response;
+        }
+    }
+}
