29ce225d015e15f656a6e965ba6c01bafe8a1b45
Santosh Singh <santosh3033@gmail.com>
2023-01-12 11:38:00 +0530

    Added IJobManageRepository and JobManageRepository repository

---
+++ Repository/IJobManageServices/IJobManageRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobManagement.Models.Dto;
+
+namespace JobManagement.Repository.IJobManageServices
+{
+    public interface IJobManageRepository
+    {
+        Task<string> CreateJob(JobDetailDto registrationDto);
+        Task<string> UpdateJob(JobDetailDto registrationDto);
+        Task<string> DeleteJob(JobDetailDto registrationDto);
+        Task<JobDetailDto> GetJob(int jobId);
+        Task<IEnumerable<JobDetailDto>> GetJobs();
+    }
+}
---
+++ Repository/JobManageServices/JobManageRepository.cs
@@ -0,0 +1,33 @@
+﻿using JobManagement.Models.Dto;
+using JobManagement.Repository.IJobManageServices;
+
+namespace JobManagement.Repository.JobManageServices
+{
+    public class JobManageRepository : IJobManageRepository
+    {
+        public Task<string> CreateJob(JobDetailDto registrationDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteJob(JobDetailDto registrationDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JobDetailDto> GetJob(int jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<JobDetailDto>> GetJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateJob(JobDetailDto registrationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
